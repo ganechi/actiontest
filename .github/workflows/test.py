64528fd887c19481355d0c66aaef41d534d56b0d
ganechi <43976382+ganechi@users.noreply.github.com>
2021-01-18 22:27:38 +0900

    Create test.py

---
+++ .github/workflows/test.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+def send_slack_webhook(text,url):
+	result = requests.post(url, data = json.dumps({
+			'text': text
+	}))
+	return result
+
+if __name__ == '__main__':
+	text = "from GitHub Actions"
+	url = "https://hooks.slack.com/services/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"
+	send_slack_webhook(text,url)

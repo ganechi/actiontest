369a51ae812e9d848e6d621fbeb94139de0d1df7
ganechi <43976382+ganechi@users.noreply.github.com>
2021-01-20 21:21:38 +0900

    Update test.py
    Update Webhook URL

--- test.py
+++ test.py
@@ -9,6 +9,6 @@ def send_slack_webhook(text,url):
 
 if __name__ == '__main__':
 	text = "from GitHub Actions"
-	url = "https://hooks.slack.com/services/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"
+	url = "https://hooks.slack.com/services/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"
 	result = send_slack_webhook(text,url)
 	print(result)
